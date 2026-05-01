98a27aeaabae89c9dd0fe7d9f61399c8ff259eca
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-30 21:25:45 -0300

    add atv02

---
+++ aula09/atividade09_pt2.py
@@ -0,0 +1,36 @@
+from utilities.operacoes_pt2 import summ, sub, mult, div
+
+#Atividade 02
+def get_nums():
+    a = float(input('Informe um número: '))
+    b = float(input('Informe outro número: '))
+    return a, b
+
+def menu():
+    dict_opt = {1: 'Soma',
+                2: 'Subtração',
+                3: 'Multiplicação',
+                4: 'Divisão'}
+    print()
+    for x, y in dict_opt.items():
+        print(f'[{x}] - {y} ')
+    choice_menu(nums)
+
+def choice_menu(nums):
+    opt = int(input('\nSelecione sua opção: '))
+    match opt:
+        case 1:
+            print(f'{summ(*nums):.2f}')
+        case 2:
+            print(f'{sub(*nums):.2f}')
+        case 3:
+            print(f'{mult(*nums):.2f}')
+        case 4: 
+            print(f'{div(*nums):.2f}')
+        case _: 
+            print('Selecione uma opção válida')
+
+
+if __name__ == '__main__':
+    nums = get_nums()
+    menu()
